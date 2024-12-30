ce3c78c409df84062503ff9d5de4c515ffe32a84
GuruXL <dillon.cordiner@gmail.com>
2024-12-30 17:10:11 -0500

    Added ReplaySavePatch

---
+++ ReplaySavePatch.cs
@@ -0,0 +1,18 @@
+﻿using BetterReplay;
+using HarmonyLib;
+using ReplayEditor;
+using SkaterXL.Data;
+using System;
+
+[HarmonyPatch(typeof(ReplayPlaybackController))]
+[HarmonyPatch("LoadReplayData", new Type[] { typeof(ReplayPlayerRecording), typeof(bool) })]
+public static class LoadReplayDataPatch
+{
+    static void Prefix(ref ReplayPlayerRecording data, bool startAtBeginning)
+    {
+        if (Main.settings.load_current_customizations)
+        {
+            data.customizations = PlayerController.Instance.characterCustomizer.CurrentCustomizations;
+        }
+    }
+}
